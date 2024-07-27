a667572ae82b0918def7b53114fc7305c9e4dc76
Yona-p <ronalynpequet1@gmail.com>
2024-07-27 20:59:35 +0200

    problem set 1 moneyball

--- problem set 1/moneyball/10.sql
+++ problem set 1/moneyball/10.sql
@@ -6,6 +6,14 @@
 --All player’s salaries
 --All player’s home runs
 --The year in which the player was paid that salary and hit those home runs
+--In 10.sql, write a query to return just such a table.
+
+--Your query should return a table with five columns, per the above.
+--Order the results, first and foremost, by player’s IDs (least to greatest).
+--Order rows about the same player by year, in descending order.
+--Consider a corner case: suppose a player has multiple salaries or performances for a given year. Order them first by number of home runs, in descending order, followed by salary, in descending order.
+--Be careful to ensure that, for a single row, the salary’s year and the performance’s year match.
+
 
 SELECT
 	p.first_name,
